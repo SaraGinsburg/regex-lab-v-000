8c67956938c002cfc676206e7a09ab1babeb1b11
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-19 11:15:29 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,17 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aeiouAEIOU]/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+\ing/)
 end
 
 def words_five_letters_long(text)
+   text.scan(/[a-zA-Z]{5}/).to_a
 
 end
 
